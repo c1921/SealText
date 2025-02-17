49b62170792fe2ae9eae4ec23f54cdf3833d8bf0
北邙 <88311117+c1921@users.noreply.github.com>
2025-02-17 11:58:52 +0800

    新增：打印版本信息

--- versioning/version.py
+++ versioning/version.py
@@ -6,5 +6,5 @@ VERSION_WITH_META = VERSION_STR
 # 应用信息
 APP_NAME = "SealText"
 COMPANY = "GitHub: c1921"
-DESCRIPTION = "SealText - 基于 Git 的加密通讯工具"
+DESCRIPTION = "基于 Git 的加密通讯工具"
 COPYRIGHT = "MIT 开源许可 - 版权所有 (c) 2025" 
